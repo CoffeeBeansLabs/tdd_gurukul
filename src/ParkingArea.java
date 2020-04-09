a30da6b3eb76a49db2cb2c3bc92ee35c4b03e3c1
Amit Basuri <amitbasuri123@gmail.com>
2020-04-09 19:29:00 +0530

    added parkingArea test cases with implementation

--- src/ParkingArea.java
+++ src/ParkingArea.java
@@ -1,5 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class ParkingArea {
-    public boolean park() {
-        return true;
+
+    private Set<Car> cars = new HashSet<Car>();
+
+
+    public void park(Car car) throws InvalidOperationException {
+        if (this.cars.contains(car)) {
+            throw  new InvalidOperationException();
+        }
+        this.cars.add(car);
+    }
+
+    public void unpark(Car car) throws InvalidOperationException {
+        if (!this.cars.contains(car)) {
+            throw new InvalidOperationException();
+        }
+        this.cars.remove(car);
     }
 }
+
+
+
+
--- test/ParkingAreaTest.java
+++ test/ParkingAreaTest.java
@@ -1,13 +1,66 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+
 
 public class ParkingAreaTest {
 
     @Test
-    public void park_should_return_true() {
-        ParkingArea  pArea = new ParkingArea();
-        boolean parked = pArea.park();
-        assertEquals(parked, true);
+    public void park_shouldReturnTrue_givenCarIsNotAlreadyParked() throws InvalidOperationException {
+        ParkingArea parkingArea = new ParkingArea();
+        Car car = new Car();
+
+        parkingArea.park(car);
+
     }
+
+    @Test(expected = InvalidOperationException.class )
+    public void park_shouldReturnFalse_givenCarIsAlreadyParked() throws InvalidOperationException {
+        ParkingArea parkingArea = new ParkingArea();
+        Car car = new Car();
+        parkingArea.park(car);
+
+        parkingArea.park(car);
+
+    }
+
+    @Test
+    public void unpark_shouldReturnTrue_givenCarIsAlreadyParked() throws InvalidOperationException {
+        ParkingArea parkingArea = new ParkingArea();
+        Car car = new Car();
+
+        parkingArea.park(car);
+
+       parkingArea.unpark(car);
+    }
+
+    @Test(expected = InvalidOperationException.class )
+    public void unpark_shouldReturnFalse_givenCarIsNotParked() throws InvalidOperationException {
+        ParkingArea parkingArea = new ParkingArea();
+        Car car = new Car();
+        parkingArea.park(car);
+
+        Car car1 = new Car();
+
+        parkingArea.unpark(car1);
+    }
+
+    @Test
+    public void park_shouldReturnTrue_multipleDifferentCarsAreNotAlreadyParked() throws InvalidOperationException {
+        ParkingArea parkingArea = new ParkingArea();
+        Car car = new Car();
+        parkingArea.park(car);
+        Car car1 = new Car();
+        parkingArea.park(car1);
+        Car car2 = new Car();
+        parkingArea.park(car2);
+        parkingArea.unpark(car1);
+        parkingArea.unpark(car);
+
+
+        Car car4 = new Car();
+
+        parkingArea.park(car4);
+
+    }
+
 }
