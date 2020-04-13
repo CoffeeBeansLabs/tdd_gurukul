28584c5f6c5a95da4ef0bb277f3f06691b96571b
Utsav <utsav@coffeebeans.io>
2020-04-13 10:20:03 +0530

    Removd print statements from function body in owner class

--- src/ParkingOwner.java
+++ src/ParkingOwner.java
@@ -1,10 +1,8 @@
 public class ParkingOwner {
 
     public void notifyParkingAreaFull() {
-        System.out.println("Parking Area full");
     }
 
     public void notifyParkingSpaceAvailable() {
-        System.out.println("Parking Area available");
     }
 }
